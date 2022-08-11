a1ea3168265c506ca7726b5a03e373fe5b6d6848
felipejnogueira <fjnog1993@gmail.com>
2022-08-11 20:24:25 -0300

    Melhorias

--- Funcoes/Users.py
+++ Funcoes/Users.py
@@ -1,22 +1,23 @@
 def inserirUser(users):
-    users[input("Digite o login do usuário: ")] = [input("Digite o nome do usuário: ").upper(),
-                                                   input("Digite a última data de acesso:" ),
+    users[input("Digite o login do usuário: ").upper()] = [input("Digite o nome do usuário: ").upper(),
+                                                   input("Digite a última data de acesso: " ),
                                                    input("Digite a última estação utilizada: ").upper()]
     print("Usuário inserido!")
 
 def pesquisarUser(users):
-    login = input("Digite o login para exibir os dados do usuário: ")
+    login = input("Digite o login para exibir os dados do usuário: ").upper()
     print("Dados: ", users.get(login))
 
 def excluirUser(users):
-    del users[input("Digite o login para excluir o usuário: ")]
+    chave = input("Digite o login para excluir o usuário: ").upper()
+    del users[chave]
+    print("Usuário excluído!")
 
 def listarUser(users):
     for chave, valor in users.items():
-        print("Dados......")
-        print("Login:", chave)
+        print("Usuário:", chave)
         print("Dados:", valor)
 
 def opcoes():
-    opcao = input("<I> - Para inserir \n<P> - Para pesquisar \n<E> - Para excluir \n<L> - Para listar usuários").upper()
+    opcao = input("<I> - Para inserir \n<P> - Para pesquisar \n<E> - Para excluir \n<L> - Para listar usuários \n").upper()
     return opcao
