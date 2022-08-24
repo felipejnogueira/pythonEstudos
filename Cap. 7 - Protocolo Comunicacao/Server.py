e20e098901151365add4d819f01cda0dd09c81c5
felipejnogueira <fjnog1993@gmail.com>
2022-08-23 23:14:32 -0300

    Inicio chat com socket

--- Cap. 7 - Protocolo Comunicacao/Server.py
+++ Cap. 7 - Protocolo Comunicacao/Server.py
@@ -1,12 +1,12 @@
 from socket import *
 
 # Ip do server e porta
-server = 127.0.0.1
+server = '127.0.0.1'
 port = 43210
 
 # Objeto de comunicação
 objSocket = socket(AF_INET, SOCK_STREAM)   #AF_INET - DNS | SOCK_STREAM - TCP / SOCK_DGRAM - UDP
-objSocket.bind((server,port))
+objSocket.bind((server, port))
 objSocket.listen(2)    # Quantidade de clients conectados simultaneamente
 
 print("Aguardando client...")
@@ -18,7 +18,7 @@ while True:
     while True:
         msgReceived = str(con.recv(1024))   # 1024 bytes
         print("Recebemos:", msgReceived)
-        msgSent = b'Ola cliente' # b - bytes | Socket se comunica apenas em bytes
+        msgSent = b"Ola cliente" # b - bytes | Socket se comunica apenas em bytes
         con.send(msgSent)
         break
     con.close()
